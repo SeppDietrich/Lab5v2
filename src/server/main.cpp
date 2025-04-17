db475cac65ca7ea49341d9814fc937ba5402ba4a
Nicolae <nicolae.iurcu@colegiu.utm.md>
2025-04-17 22:24:10 +0300

    Commit

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -8,145 +8,121 @@
 #include <string>
 #include <algorithm>
 
-
 #define PORT 8080
 #define MAX_CLIENTS 10
 
-struct User{
+struct User {
     int socket;
     std::string username;
-    User(int socket): socket(socket){}
-
+    User(int socket) : socket(socket) {}
 };
 
-
-class Server
-{
+class Server {
 public:
     std::vector<User*> users;
-
     int serverSocket, clientSocket;
     struct sockaddr_in serverAddr, clientAddr;
     socklen_t addrLen = sizeof(clientAddr);
-    Server();
-    ~Server();
-    void sendMessage(const std::string& message, int socket){
+
+    Server() {}
+    ~Server() {
+        for (auto user : users) {
+            delete user;
+        }
+    }
+
+    void sendMessage(const std::string& message, int socket) {
         int bytesSent = send(socket, message.c_str(), message.size(), 0);  
         if (bytesSent < 0) {
             std::cerr << "Error sending message to client" << std::endl;
         }  
     }
-    bool authenticationSuccess(std::string sentUsername, User user) {  
-        
-        if(sentUsername.length()>100){
+
+    bool authenticationSuccess(std::string sentUsername, User* user) {  
+        if (sentUsername.length() > 100) {
             return false;
         }
-        user.username = sentUsername;  
-        std::cout<<"Client authenticated as : " <<user.username<<std::endl;
+        user->username = sentUsername;  
+        std::cout << "Client authenticated as: " << user->username << std::endl;
         return true;
     }
 
-
-    void parseMessage(std::string message, std::string &returnMessage, User user){
+    void parseMessage(std::string message, std::string &returnMessage, User* user) {
         message.erase(message.find_last_not_of(" \t\n\r\f\v") + 1);
-        if(message[0]!='/'){
-            std::string manual=
+        if (message[0] != '/') {
+            std::string manual =
                 "/auth [username] - authenticate \n"
                 "/join [chatNumber] - join chat \n"
-                "/message [message] -message sent into chat \n"
-                "/leave -leave chat \n";
+                "/message [message] - message sent into chat \n"
+                "/leave - leave chat \n";
             
-            returnMessage= manual;
+            returnMessage = manual;
+            return;
         }
+
         int spacePos = message.find(' ');
         std::string command = message.substr(0, spacePos);
         std::string data = (spacePos != std::string::npos) ? message.substr(spacePos + 1) : "";
 
-        
-        switch(command[1]) {
-            case 'a':{ // /auth
-                if(authenticationSuccess(data, user)) {
-                    returnMessage= "Successfully logged in as " + data + "\n";
-                }
-                returnMessage= "Authentication Failed";
+        if (command == "/auth") {
+            if (authenticationSuccess(data, user)) {
+                returnMessage = "Successfully logged in as " + data + "\n";
+            } else {
+                returnMessage = "Authentication Failed";
             }
-                
-            // case 'j':{ // /join
-            //     int roomId=std::stoi(data);
-            //     roomHandler.joinRoom(this, data);
-
-            //     return "Joining chat: " + data;
-            // }
-            // case 'm':{ // /message
-                
-            //     return "Sending message: " + data;
-            // }
-            // case 'l':{ // /leave
-            //     return "Leaving current chat";
-            // }
-            default:{
-                returnMessage="Unknown command: " + command;
-            }
+        } else {
+            returnMessage = "Unknown command: " + command;
         }
-
-        
     }
 
-    void run(){
-        while (true) {
-            clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
-            if (clientSocket < 0) {
-                std::cerr << "Accept failed." << std::endl;
-                continue;
-            }
-            users.emplace_back(clientSocket);
-            char clientIP[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
-            std::cout << "New client connected from " << clientIP << ":" << ntohs(clientAddr.sin_port) << std::endl;
-
-            std::thread clientThread(handleClient, clientSocket, users.end());
-            clientThread.detach();
-        }
-
-        close(serverSocket);
-    }
-    
-    void static handleClient(int socket, User user) {
+    void handleClient(User* user) {
         char buffer[1024];
-
 
         while (true) {
             memset(buffer, 0, sizeof(buffer));
-            int bytesRead = read(socket, buffer, sizeof(buffer));
+            int bytesRead = read(user->socket, buffer, sizeof(buffer));
             if (bytesRead <= 0) {
                 std::cout << "Client disconnected." << std::endl;
                 break;
             }
             buffer[bytesRead] = '\0';
 
-            
-            // Trim any trailing newlines or whitespace
             std::string message(buffer);
+            std::string response;
             
-            
-            
-            parseMessage(message, message, user);
-        
-            //std::cout << fullMessage << "\n";
-            
-            sendMessage(message, socket); 
+            parseMessage(message, response, user);
+            sendMessage(response, user->socket); 
         }
-        close(socket);
+        close(user->socket);
+    }
+
+    void run() {
+        while (true) {
+            clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
+            if (clientSocket < 0) {
+                std::cerr << "Accept failed." << std::endl;
+                continue;
+            }
+
+            User* newUser = new User(clientSocket);
+            users.push_back(newUser);
+
+            char clientIP[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
+            std::cout << "New client connected from " << clientIP << ":" << ntohs(clientAddr.sin_port) << std::endl;
+
+            std::thread clientThread(&Server::handleClient, this, newUser);
+            clientThread.detach();
+        }
+
+        close(serverSocket);
     }
     
-    void initialize(){
-
-        
-
+    void initialize() {
         serverSocket = socket(AF_INET, SOCK_STREAM, 0);
         if (serverSocket < 0) {
             std::cerr << "Socket creation failed." << std::endl;
-            return ;
+            return;
         }
 
         serverAddr.sin_family = AF_INET;
@@ -155,30 +131,21 @@ public:
 
         if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
             std::cerr << "Bind failed." << std::endl;
-            return ;
+            return;
         }
 
         if (listen(serverSocket, MAX_CLIENTS) < 0) {
             std::cerr << "Listen failed." << std::endl;
-            return ;
+            return;
         }
 
         std::cout << "Server is listening on port " << PORT << std::endl;
     }
-
-    
 };
-
-
-
-
-
 
 int main() {
     Server server;
     server.initialize();
     server.run();
-
-    
     return 0;
 }
