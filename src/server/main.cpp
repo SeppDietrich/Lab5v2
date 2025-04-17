02ba9661dd496359a7f69b21b36cfdd3d9144079
Nicolae <nicolae.iurcu@colegiu.utm.md>
2025-04-17 19:45:47 +0300

    Commit

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -12,14 +12,40 @@
 #define PORT 8080
 #define MAX_CLIENTS 10
 
-void sendMessage(const std::string& message, int socket){
-        int bytesSent = send(socket, message.c_str(), message.size(), 0);  
-        if (bytesSent < 0) {
-            std::cerr << "Error sending message to client" << std::endl;
-        }  
-    }
+struct User{
+    int socket;
+    std::string username;
 
-void handleClient(int socket) {
+}
+
+
+class Server
+{
+public:
+    std::vector<User*> users;
+    Server();
+    ~Server();
+
+    void run(){
+        while (true) {
+            clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
+            if (clientSocket < 0) {
+                std::cerr << "Accept failed." << std::endl;
+                continue;
+            }
+
+            char clientIP[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
+            std::cout << "New client connected from " << clientIP << ":" << ntohs(clientAddr.sin_port) << std::endl;
+
+            std::thread clientThread(handleClient, clientSocket);
+            clientThread.detach();
+        }
+
+        close(serverSocket);
+    }
+    
+    void handleClient(int socket) {
         char buffer[1024];
 
         while (true) {
@@ -45,49 +71,52 @@ void handleClient(int socket) {
         }
         close(socket);
     }
+    
+    void initialize(){
 
-int main() {
-    int serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addrLen = sizeof(clientAddr);
+        int serverSocket, clientSocket;
+        struct sockaddr_in serverAddr, clientAddr;
+        socklen_t addrLen = sizeof(clientAddr);
 
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket < 0) {
-        std::cerr << "Socket creation failed." << std::endl;
-        return -1;
-    }
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(PORT);
-
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        std::cerr << "Bind failed." << std::endl;
-        return -1;
-    }
-
-    if (listen(serverSocket, MAX_CLIENTS) < 0) {
-        std::cerr << "Listen failed." << std::endl;
-        return -1;
-    }
-
-    std::cout << "Server is listening on port " << PORT << std::endl;
-
-    while (true) {
-        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
-        if (clientSocket < 0) {
-            std::cerr << "Accept failed." << std::endl;
-            continue;
+        serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (serverSocket < 0) {
+            std::cerr << "Socket creation failed." << std::endl;
+            return -1;
         }
 
-        char clientIP[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
-        std::cout << "New client connected from " << clientIP << ":" << ntohs(clientAddr.sin_port) << std::endl;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_port = htons(PORT);
 
-        std::thread clientThread(handleClient, clientSocket);
-        clientThread.detach();
+        if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+            std::cerr << "Bind failed." << std::endl;
+            return -1;
+        }
+
+        if (listen(serverSocket, MAX_CLIENTS) < 0) {
+            std::cerr << "Listen failed." << std::endl;
+            return -1;
+        }
+
+        std::cout << "Server is listening on port " << PORT << std::endl;
     }
 
-    close(serverSocket);
+    
+};
+
+
+void sendMessage(const std::string& message, int socket){
+        int bytesSent = send(socket, message.c_str(), message.size(), 0);  
+        if (bytesSent < 0) {
+            std::cerr << "Error sending message to client" << std::endl;
+        }  
+    }
+
+
+
+int main() {
+    
+
+    
     return 0;
 }
